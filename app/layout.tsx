75a8917d7f3b4f31dfee395ac3ef0f48ef76a8a8
IQBS <rakotisaigayathri@gmail.com>
2026-07-01 18:37:20 +0530

    design: redesign color tokens, set global typography to Inter, and add premium glassmorphic navigation bar styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         {/* Font Awesome */}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
         {/* Google Fonts */}
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
       </head>
       <body>
         {children}
